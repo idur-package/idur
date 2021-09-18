a4ea908bdad09dfcc7bd60059aa638b0a9f33abc
Manuel González <67566612+Can202@users.noreply.github.com>
2021-09-18 08:06:27 -0300

    test install file
    this is made is phone xD
    i can't do use the computer right now, cause i'm far

---
+++ install.bash
@@ -0,0 +1,24 @@
+#/usr/bin/bash
+# test
+if [ ! $(whoami) = "root" ]
+then
+    echo "you need root"
+    exit
+fi
+if [ ! -f "/usr/bin/apt" ]
+then
+    echo "idur works only on debian"
+    exit
+fi
+
+cd /tmp/
+rm -vrf idurtemp
+mkdir -p idurtemp
+cd idurtemp/
+curl -LO #link
+chmod a+x idur.py
+./idur.py add-repo official-repo #link
+./idur.py install idur-stable
+cd ..
+rm -vrf idurtemp
+cd $(basename $0) #not remember
