c848d1ba43ba5e3a9103ae7c8d3c38a30d367ca9
Can202 <mgoopazo@gmail.com>
2021-09-21 08:19:16 -0300

    no check remove in update

--- src/idur.py
+++ src/idur.py
@@ -497,7 +497,7 @@ def update_package(packagename):
 			exit()
 		if package.Version < newpackage.Version:
 			print("update")
-			remove_package(packagename)
+			remove_package(packagename, check=False)
 			install_package(packagename)
 	else:
 		print("you don't have installed " + packagename)
